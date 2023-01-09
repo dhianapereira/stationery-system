f68e6a4ff347899eee09a7b6ae280f9954fc133d
Dhiana Pereira <dhianaslv@outlook.com>
2023-01-08 22:41:23 -0300

    feat: Creates users table

--- system/src/main/java/data/database.sql
+++ system/src/main/java/data/database.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE stationery;
+USE stationery;
+
+CREATE TABLE users(
+    cpf VARCHAR(11) NOT NULL,
+    fullName VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    isActive BOOL DEFAULT 1,
+    isAdmin BOOL DEFAULT 0,
+    password VARCHAR(255) NOT NULL,
+    CONSTRAINT users_pk PRIMARY KEY (cpf)
+);
